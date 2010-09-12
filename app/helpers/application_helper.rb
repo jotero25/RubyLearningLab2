f5a5aaed296d73651c5ad822fd439ad7d8bb8e4c
Jesus Otero <jotero25@gmail.com>
2010-09-11 19:00:42 -0500

    After dynamics routing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #return a title in a per-page basis
+  def title
+    base_title = "Ruby on Rails Labs"
+    if @title.nil?
+      base_title
+    end
+    "#{base_title} | #{@title}"
+  end
 end
